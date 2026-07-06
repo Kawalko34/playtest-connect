2d21140d2cde9774cb255b607e8f8901aded68f3
Kawalko34 <kawalko334411@gmail.com>
2026-07-06 01:42:27 +0100

    Rename app to Play Test

--- Start Play Test.bat
+++ Start Play Test.bat
@@ -1,8 +1,9 @@
 @echo off
-title PlayTest Connect - server
+title Play Test - server
 cd /d "%~dp0"
 rem Start a local server (Google sign-in requires http://, not file://)
-start /min "PlayTest Connect server" cmd /c "npx -y http-server -p 8377 -c-1"
+start /min "Play Test server" cmd /c "npx -y http-server -p 8377 -c-1"
 timeout /t 2 /nobreak >nul
 start "" http://localhost:8377
 exit
+
